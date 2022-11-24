c50eb0a354a7c75927c19cfb795eee6b1dace4dc
GitHub Action <action@github.com>
2022-11-24 09:21:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/leleworld/intellijplatformtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/leleworld/intellijplatformtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.leleworld.intellijplatformtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leleworld.intellijplatformtemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/leleworld/intellijplatformtemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/leleworld/intellijplatformtemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.leleworld.intellijplatformtemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leleworld.intellijplatformtemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
